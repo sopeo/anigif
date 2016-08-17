7a38303b7c0398bbfc7ccb3d56ae3211fb0558f9
福士 宗介 <fukushi@camobile.com>
2016-08-17 14:22:13 +0900

    Initial Commit

---
+++ anigif/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  anigif
+//
+//  Created by 福士 宗介 on 2016/08/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
